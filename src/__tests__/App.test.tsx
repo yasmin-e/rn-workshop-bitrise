91a8dad6b38d1eaf71bb1ed1cdf42e0bab6f29aa
Fodor Dániel <daniel.fodor@benestudio.co>
2021-10-28 11:16:55 +0200

    add test

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,11 +1,28 @@
-import { fireEvent, render } from '@testing-library/react-native';
+import { fireEvent, render, waitFor } from '@testing-library/react-native';
 import App from '../App';
 import React from 'react';
 import { maxLives, maxQuestions } from '../config';
 import fetch from 'jest-fetch-mock';
 import { IResponse } from '../interfaces';
 
+const res: IResponse = {
+  response_code: 0,
+  results: [
+    {
+      category: '1',
+      correct_answer: 'x',
+      difficulty: 'easy',
+      incorrect_answers: ['incorrect', 'b', 'c'],
+      question: 'This is a question',
+      type: 'multiple',
+    },
+  ],
+};
+
 describe('Game', () => {
+  beforeAll(() => {
+    fetch.mockResponse(JSON.stringify(res));
+  });
   it('should have maximum lives by default', async () => {
     const { findAllByTestId } = render(<App />);
     const hearts = await findAllByTestId('heart-full');
@@ -20,20 +37,6 @@ describe('Game', () => {
   });
 
   it('should decrease lives when wrong answer given', async () => {
-    const res: IResponse = {
-      response_code: 0,
-      results: [
-        {
-          category: '1',
-          correct_answer: 'x',
-          difficulty: 'easy',
-          incorrect_answers: ['incorrect', 'b', 'c'],
-          question: 'This is a question',
-          type: 'multiple',
-        },
-      ],
-    };
-    fetch.mockResponseOnce(JSON.stringify(res));
     const { findByTestId, getByTestId, getAllByTestId } = render(<App />);
     const title = await findByTestId('question');
     const inCorrectButton = getByTestId('answer-1');
@@ -46,5 +49,20 @@ describe('Game', () => {
     const emptyHearts = getAllByTestId('heart-empty');
     expect(hearts).toHaveLength(maxLives - 1);
     expect(emptyHearts).toHaveLength(1);
+  });
+
+  it('should increase step number if correct answer given', async () => {
+    const { getByTestId, findByTestId, findByText, getAllByTestId } = render(
+      <App />
+    );
+    const correctButton = await findByTestId('answer-0');
+
+    fireEvent.press(correctButton);
+
+    waitFor(() => {
+      expect(getByTestId('currentStep').children[0]).toBe(
+        `2 / ${maxQuestions}`
+      );
+    });
   });
 });
