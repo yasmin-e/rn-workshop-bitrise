ec3ac8de71d050b7408d5620899e1b6f189fddbd
Fodor Dániel <daniel.fodor@benestudio.co>
2021-11-02 08:53:01 +0100

    move Game test

--- src/__tests__/Game.test.tsx
+++ src/__tests__/Game.test.tsx
@@ -1,7 +1,7 @@
 // jest.mock('../utils/helpers');
 import React from 'react';
 import { render } from '@testing-library/react-native';
-import { Game } from '.';
+import { Game } from '../screens';
 import { IQuiz } from '../interfaces';
 
 jest.mock('../utils/helpers', () => {
